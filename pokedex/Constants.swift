4e67637ecd2ace089ac431384d493ad5e5fcf059
Brice Dreamer <BriceDreamer@Brices-MacBook-Pro.local>
2016-10-12 16:22:39 +0200

    we just finished our frikkin pokedex appgit status woohoooo

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Brice Dreamer on 10/12/16.
+//  Copyright © 2016 Brice Dreamer. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
